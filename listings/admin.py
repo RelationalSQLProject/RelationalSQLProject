acb822052759d6b2329745dcf0ad713c210ca1f1
stevenjchang <stevenjchang@gmail.com>
2020-06-05 18:38:17 -0700

    register Listing to admin view

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from listings.models import Listing, Product, Bid
 
 # Register your models here.
+admin.site.register(Listing)
+admin.site.register(Product)
+admin.site.register(Bid)
